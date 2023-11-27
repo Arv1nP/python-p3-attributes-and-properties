d25fcced7f7813d19c99969f48e77b5849f4ff72
Arvin Pourabdollah <Arvinpourabdollah@gmail.com>
2023-11-27 15:26:19 +1100

    complete

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,13 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name="", breed=""):
+        self.name = name
+        self.breed = breed
+
+        if not isinstance(name, str) or not (1 <= len(name) <= 25):
+            print("Name must be string between 1 and 25 characters.")
+        elif not isinstance(breed, str) or breed not in APPROVED_BREEDS:
+            print("Breed must be in list of approved breeds.")
+        else:
+            print("Dog created successfully!")
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,19 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name="", job=""):
+        self.name = name
+        self.job = job
+
+        if not isinstance(name, str) or not (1 <= len(name) <= 25):
+            print("Name must be a string between 1 and 25 characters.")
+        elif not isinstance(job, str) or job not in APPROVED_JOBS:
+            print("Job must be in list of approved jobs.")
+        elif not job:
+            print("Job must be provided.")
+        else:
+            self.name = self.format_name(name)
+            print("Person created successfully!")
+
+    def format_name(self, name):
+        return " ".join([part.capitalize() for part in name.split()])
